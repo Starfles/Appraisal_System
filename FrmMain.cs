465fe597ac69bcd91495c66215b1008dabafdbca
Starfles <tremostar@outlook.com>
2024-04-10 19:50:58 +0800

    提交文件

---
+++ FrmMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appraisal_System
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FrmUserManager frmUserManager = new FrmUserManager();
+            frmUserManager.MdiParent = this;
+            frmUserManager.Parent = splitContainer1.Panel2;
+            frmUserManager.Show();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
